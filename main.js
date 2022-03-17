4614178271c662daf4d12b75e7bde25465db04f2
Amir Moghal <5mogha22@solent.ac.uk>
2022-03-17 15:02:55 +0000

    completed first banner and change

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 document.getElementById("cookie").style.display = "block";
+
+document.getElementById("Nope").addEventListener("click", e=>{
+    document.getElementById("cookie").style.display = "none";
+});
+
+
+document.getElementById("Yes").addEventListener("click", e=>{
+    document.getElementById("cookie").innerHTML = "<h3>First Lesson - Don't admit that</h3><p>But you did So your reward your third year will now feature 30% extra dementor!</p>";
+});
